76afabb1d817f22c9366b78474624f22cf546615
Justin Hawkins <justin@hawkins.id.au>
2019-06-20 21:45:48 +0930

    Handle zoom levels

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # slicerdicer
+
+Slice and dice an image, turning it into many equal sized tiles.
+
+## Usage
+
+slicerdicer -help
+
+slicerdicer --filename foo.png --tile-size 256
--- main.go
+++ main.go
@@ -31,27 +31,49 @@ func main() {
 
   fmt.Println("starting tiling")
 
-  for y := 0 ; y <= (size.Y / tile_size_y) ; y++ {
+  z := 0
 
-    for x := 0 ; x <= (size.X / tile_size_x) ; x++ {
- 
-      output_filename := fmt.Sprintf("tile-0-%d-%d.png", x, y)
-      cropped := imaging.Crop(src, image.Rect(tile_size_x*x, tile_size_y*y, tile_size_x*x+tile_size_x, tile_size_y*y+tile_size_y));
-
-      fmt.Print("writing to: ", output_filename, "        ");
-      fmt.Print("\r")
-
-      writer, _ := os.Create(output_filename)
-      err = png.Encode(writer, cropped)
-      writer.Close()
-      runtime.GC()
-      if err != nil {
-        fmt.Println(err)
+  // outer loop for zoom
+  for {
+    if (z == 0) {
+      // do nothing
+    } else {
+      // halve image size
+      src = imaging.Resize(src, size.X/2, 0, imaging.Linear)
+      // recalculate size
+      size = src.Bounds().Max
+      // we are done if we are now smaller then the tile
+      if (size.X < tile_size_x || size.Y < tile_size_y) {
+        break;
       }
     }
+
+    fmt.Print(fmt.Sprintf("zoom level: %d (%d x %d)\n", z, size.X, size.Y))
+
+    for y := 0 ; y <= (size.Y / tile_size_y) ; y++ {
+
+      for x := 0 ; x <= (size.X / tile_size_x) ; x++ {
+ 
+        output_filename := fmt.Sprintf("tile-%d-%d-%d.png", z, x, y)
+        cropped := imaging.Crop(src, image.Rect(tile_size_x*x, tile_size_y*y, tile_size_x*x+tile_size_x, tile_size_y*y+tile_size_y));
+
+        fmt.Print("writing to: ", output_filename, "        ");
+        fmt.Print("\r")
+
+        writer, _ := os.Create(output_filename)
+        err = png.Encode(writer, cropped)
+        writer.Close()
+        runtime.GC()
+        if err != nil {
+          fmt.Println(err)
+        }
+      }
+    }
+
+    fmt.Println()
+    z++
   }
 
-  fmt.Println()
   fmt.Println("done")
 
 }
